d0d114cf1532e6583666ff84c066493212ab69b3
Brian Lobdell <bri.lobdell@gmail.com>
2014-01-13 11:21:11 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Zombietwitter::Application.config.secret_key_base = '58b2edc56a056f1dbb4e91d45e10da7ca30f65da80c619f03566199c2da4349b4b5f4b5b25601c8f4857f4eb764a49bc197ff50cbebf4ec6cbcf61d5b6124d15'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Zombietwitter::Application.config.session_store :cookie_store, key: '_zombietwitter_session'
